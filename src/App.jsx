53b06c7de757f510c99fc0e48c367732930dcbaa
raushan <kraushan022@gmail.com>
2025-04-15 00:05:26 +0530

    tree-view-strucutre project

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,26 @@ import Accordion from "./components/accordion";
 import ImageSlider from "./components/image-slider";
 import LoadMoreData from "./components/load-more-data";
 import RandomColor from "./components/random-color";
+import TreeView from "./components/tree-view";
+import menus from "./components/tree-view/data";
 
 function App() {
   return (
     <>
       {/* <Accordion /> */}
+
       {/* <RandomColor /> */}
+
       {/* <ImageSlider
         url={"https://picsum.photos/v2/list"}
         page={"1"}
         limit={"5"}
       /> */}
-      <LoadMoreData />
+
+      {/* <LoadMoreData /> */}
+
+      {/* Tree View Component/ menu UI component/ recursive navigation menu */}
+      <TreeView menus={menus} />
     </>
   );
 }
